33fc288e2731cafd171056bdbbfad1e46bb82fa0
steven <stevenle2011@gmail.com>
2017-05-04 15:25:55 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+app.use(morgan('dev'));
+
+var port = (process.env.PORT || 3000);
+
+app.use(express.static(__dirname +'/public'));
+
+app.get('/whoami',function(req,res){
+	var re = /\(([^)]+)\)/;
+
+	var ip = req.ip
+	var lang = req.acceptsLanguages()[0];
+	var software = re.exec(req.headers['user-agent'])[1]
+
+	res.json({
+		"ip-address": ip,
+		"language": lang,
+		"software": software
+	})
+})
+
+
+
+app.listen(port,function(){
+	console.log(`Listening on port ${port}`)
+})
